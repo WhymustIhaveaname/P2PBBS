1681775cc17006f11873a1f11dfef258eb06c5d0
WhymustIhaveaname <38423345+WhymustIhaveaname@users.noreply.github.com>
2018-06-06 14:42:47 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -31,14 +31,24 @@ public class Test{
     }
 
     public static void testHB(){
-        Server ser = new Server(Server.UDP, 3333);
+        Server ser0 = new Server(Server.UDP, 3333);
+        Thread t0 = new Thread(ser0);
+        t0.start();
+        try{Thread.sleep(300);}catch(Exception e){e.printStackTrace();}
+        Server ser = new Server(Server.HEARTBEATSERVER, 3333);
         Thread t = new Thread(ser);
         t.start();
+    }
+    public static void startUDP(){
+      Server ser0 = new Server(Server.UDP, 3333);
+      Thread t0 = new Thread(ser0);
+      t0.start();
     }
     public static void main(String args[]){
         //Test.testRPL();
         //Test.testRP();
         //Test.testReceiveUDP();
-        Test.testHB();
+        //Test.testHB();
+        Test.startUDP();
     }
 }
